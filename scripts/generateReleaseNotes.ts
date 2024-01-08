52d2c91ec40be70b6d20f26627df7ee89a42ce17
David Sherret <dsherret@users.noreply.github.com>
2024-01-08 18:52:08 -0500

    chore: add generated change log to releases (#598)

---
+++ scripts/generateReleaseNotes.ts
@@ -0,0 +1,37 @@
+import { generateChangeLog } from "https://raw.githubusercontent.com/dprint/automation/0.9.0/changelog.ts";
+
+const version = Deno.args[0];
+const changelog = await generateChangeLog({
+  versionTo: version,
+});
+const text = `${changelog}\n
+## Install
+
+[Install](https://dprint.dev/install/) and [setup](https://dprint.dev/setup/) dprint.
+
+Then in your project's dprint configuration file:
+
+1. Specify the plugin url in the \`"plugins"\` array.
+2. Add a \`"typescript"\` configuration property if desired.
+   \`\`\`jsonc
+   {
+     // ...etc...
+     "typescript": {
+       // TypeScript & JavaScript config goes here
+     },
+     "excludes": [
+       "**/node_modules"
+     ],
+     "plugins": [
+       "https://plugins.dprint.dev/typescript-${version}.wasm"
+     ]
+   }
+   \`\`\`
+
+## JS Formatting API
+
+* [JS Formatter](https://github.com/dprint/js-formatter) - Browser/Deno and Node
+* [npm package](https://www.npmjs.com/package/@dprint/typescript)
+`;
+
+console.log(text);
